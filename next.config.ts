9c927423d27607b53bfe48d95d1a65635eabb533
R0X4R <32596297+R0X4R@users.noreply.github.com>
2025-03-10 21:38:14 +0530

    fixed typo

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,15 @@
+import type { NextConfig } from "next"
+
 const nextConfig: NextConfig = {
     output: "export",
     distDir: "out",
     images: { unoptimized: true },
-    eslint: { ignoreDuringBuilds: true },
-    typescript: { ignoreBuildErrors: true },
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    typescript: {
+    ignoreBuildErrors: true,
+  },
 }
 
-export default nextConfig;
+export default nextConfig
