97bdfe6b34e8300a4826d5ae6b71f01e1b29461f
R0X4R <32596297+R0X4R@users.noreply.github.com>
2025-03-21 15:53:33 +0530

    an error fixed
    fixed few errors

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
     output: "export",
+    reactStrictMode: true,
     distDir: "out",
     images: { unoptimized: true },
     eslint: {
