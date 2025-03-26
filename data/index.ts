acf29378d498f8572aad20444be5509bed69f777
R0X4R <32596297+R0X4R@users.noreply.github.com>
2025-03-27 01:25:12 +0530

    added more mantras
    Added more vedic wisdom

--- data/index.ts
+++ data/index.ts
@@ -68,34 +68,65 @@ export const vedicQuotes = [
         reference: "ऋ 10.53.6",
         meaning: "Be a true human.",
     },
-]
+    {
+        id: 11,
+        mantra: "देव॑ सवितः॒ प्र सु॑व य॒ज्ञं प्र सु॑व य॒ज्ञप॑तिं॒ भगा॑य।दि॒व्यो ग॑न्ध॒र्वः॑ के॑त॒पू केतं॑ नः पुनातु वा॒चस्पति॒र्वाचं॑ नः स्वदतु",
+        refernce: "य 30.1",
+        meaning: "O Creator God, help our sacrifice succeed and bring prosperity. Let the protector of the earth cleanse our thoughts, and may the Lord of speech make our words kind and sweet.",
 
-export const articles = [
-    {
-        title: "The Origin of Mother Ganga: Popular Stories & Logical Truth",
-        description:
-            "Explore the historical and scriptural origins of Mother Ganga, referenced in the Ramayana.",
-        image: "/images/ganga.jpg",
-        author: "Eshan Singh",
-        date: "11 March, 2025",
-        link: "/blog/the-origin-of-mother-ganga-popular-stories-and-logical-truths",
     },
     {
-        title: "Lord Shiva: The Historical & Scriptural Legacy of Mahadev",
-        description:
-            "Discover Lord Shiva's significance in ancient scriptures like the Mahabharata and Shanti Parva.",
-        image: "/images/shiva.jpg",
-        author: "Eshan Singh",
-        date: "26 February, 2025",
-        link: "/blog/lord-shiva-the-historical-and-scriptural-legacy-of-mahadev",
+        id: 12,
+        mantra: "तत्स॑वि॒तुर्वरे॑ण्यं॒ भर्गो॑ दे॒वस्य॑ धीमहि।धियो॒ यो नः॑ प्रचो॒दया॑त्",
+        reference: "य 30.2",
+        meaning: "May we be filled with the purest divine light of our Creator, so He awakens and guides our minds with wisdom",
     },
     {
-        title: "Lord Krishna: His True Identity, Was Radha Real & Was He a Romantic Figure",
-        description:
-            "Was Lord Krishna a divine figure or a romantic hero? Explore ancient scriptures for the truth.",
-        image: "/images/barcelona.jpg",
-        author: "Eshan Singh",
-        date: "Coming Soon",
-        link: "#",
+        id: 13,
+        mantra: "विश्वा॑नि देव सवितर्दुरि॒तानि॒ परा॑ सुव। यद्भ॒द्रं तन्न॒ऽआ सु॑व",
+        reference: "य 30.3",
+        meaning: "O Divine Creator, take away all our troubles and sins, and bless us with goodness and prosperity",
+    },
+    {
+        id: 14,
+        mantra: "यऽआ॑त्म॒दा ब॑ल॒दा यस्य॒ विश्व॑ऽउ॒पास॑ते प्र॒शिषं॒ यस्य॑ दे॒वाः।यस्य॑ छा॒याऽमृतं॒ यस्य॑ मृ॒त्युः कस्मै॑ दे॒वाय॑ ह॒विषा॑ विधेम",
+        reference: "य 25.13",
+        meaning: "O Lord, you give life to our souls, filling them with strength and hope. Every divine being sings joyful songs about your amazing glory. Your caring protection brings us everlasting life, and without you, there would be nothing. We offer our heartfelt praise and worship through happy hymns.",
+    },
+    {
+        id: 15,
+        mantra: "यः प्रा॑ण॒तो नि॑मिष॒तो म॑हि॒त्वैक॒ऽइद्राजा॒ जग॑तो ब॒भूव॑। यऽईशे॑ऽअ॒स्य द्वि॒पद॒श्चत॒ु॑ष्पदः॒ कस्मै॑ दे॒वाय॑ ह॒विषा॑ विधेम",
+        reference: "य 23.3",
+        meaning: "He, by His greatness, rules over all that lives, moves, and sees. He is the Lord of all people and animals. To Him, we offer our prayers and devotion",
+    },
+    {
+        id: 16,
+        mantra: "हि॒र॒ण्य॒ग॒र्भः सम॑वर्त्त॒ताग्रे॑ भू॒तस्य॑ जा॒तः पति॒रेक॑ऽ आसीत्। स दा॑धार पृथि॒वीं द्यामु॒तेमां कस्मै॑ दे॒वाय॑ ह॒विषा॑ विधेम",
+        reference: "य 13.4",
+        meaning: "Before everything, the Lord, who carries all the stars within Him, existed everywhere. He alone ruled over all creation. He holds both the earth and the heavens. To Him, we offer our prayers and devotion.",
+    },
+    {
+        id: 17,
+        mantra: "येन॒ द्यौरु॒ग्रा पृ॑थि॒वी च॑ द्य्॒ढा येन॒ स्व स्तभि॒तं येन॒ नाकः॑।योऽ अ॒न्तरि॑क्षे॒ रज॑सो वि॒मानः॒ कस्मै॑ दे॒वाय॑ ह॒विषा॑ विधेम",
+        reference: "य 32.6",
+        meaning: "He who lights up the sky and makes the earth steady, who upholds the heavens and the realm of light, and who measures the vast spaces in between—to that Lord, we offer our prayers and devotion.",
+    },
+    {
+        id: 18,
+        mantra: "स॒त्यं च॑ मे श्र॒द्धा च॑ मे॒ जग॑च्च मे॒ धनं॑ च मे॒ विश्वं॑ च मे॒ मह॑श्च मे क्री॒डा च॑ मे॒ मोद॑श्च मे जा॒तं च॑ मे जनि॒ष्यमा॑णं च मे सू॒क्तं च॑ मे सुकृ॒तं च॑ मे य॒ज्ञेन॑ कल्पन्ताम्",
+        reference: "य 18.5",
+        meaning: "May my honesty and faith, my wealth and possessions, my strength and greatness, my joys and pleasures, my children and future generations, my kind words and good deeds all be protected and blessed through my offerings.",
+    },
+    {
+        id: 19,
+        mantra: "प्र शंत॑मा॒ वरु॑णं॒ दीधि॑ती॒ गीर्मि॒त्रं भग॒मदि॑तिं नू॒नम॑श्याः। पृष॑द्योनिः॒ पञ्च॑होता शृणो॒त्वतू॑र्तपन्था॒ असु॑रो मयो॒भुः",
+        reference: "ऋ 5.42.1",
+        meaning: "O wise one! Use and cherish the speech that uplifts the vital breath, brings great joy, and fills life with happiness. It is embraced by the five vital energies and connects to wealth, the sky, and the earth. Listen to the divine sound in the clouds, which follows an unbreakable path and brings true happiness",
+    },
+    {
+        id: 20,
+        mantra: "त्वं वरु॑ण उ॒त मि॒त्रो अ॑ग्ने॒ त्वां व॑र्धन्ति म॒तिभि॒र्वसि॑ष्ठाः। त्वे वसु॑ सुषण॒नानि॑ सन्तु यू॒यं पा॑त स्व॒स्तिभिः॒ सदा॑ नः",
+        reference: "ऋ 7.12.3",
+        meaning: "O Lord of light and life, Agni, you are our judge and our friend. Those blessed with peace and prosperity praise you with wisdom and devotion. May those who love and trust you receive your generous blessings. And may all wise ones and leaders protect and guide us toward peace, prosperity, and well-being",
     },
 ]
