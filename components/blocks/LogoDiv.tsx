a07b05f4ecc2e80685a0f5393a08802fecc0d1af
R0X4R <32596297+R0X4R@users.noreply.github.com>
2025-04-06 00:25:42 +0530

    Update LogoDiv.tsx

--- components/blocks/LogoDiv.tsx
+++ components/blocks/LogoDiv.tsx
@@ -1,6 +1,7 @@
 "use client"
 import Image from "next/image"
 import dynamic from "next/dynamic"
+import logo from "@/public/images/logo.png"
 
 const DailyVedicQuote = dynamic(
     () => import("@/components/blocks/VedicQuotes"),
@@ -12,18 +13,19 @@ const DailyVedicQuote = dynamic(
 export default function LogoDiv() {
     return (
         <header className="mb-14 flex flex-col items-center justify-center text-center">
-            <Image
-                src="/images/logo.png"
-                sizes="100vw"
-                height={0}
-                width={0}
-                title="Satya Vivechan Logo"
-                quality={100}
-                priority
-                // layout="responsive"
-                className="pointer-events-none h-full w-62 rounded-full object-cover select-none"
-                alt="logo"
-            />
+            <div className="relative size-62 overflow-hidden rounded-full">
+                <Image
+                    src={logo}
+                    sizes="100vw"
+                    title="Satya Vivechan Logo"
+                    quality={100}
+                    priority
+                    placeholder="blur"
+                    fill
+                    className="pointer-events-none rounded-full object-contain select-none"
+                    alt="logo"
+                />
+            </div>
             <h1 className="flex flex-col items-center justify-center text-6xl font-black tracking-tighter text-lime-950 uppercase selection:bg-lime-950 selection:text-lime-50 md:text-8xl dark:text-stone-50 dark:selection:bg-stone-50 dark:selection:text-stone-950">
                 Satya Vivechan
                 <span className="text-sm font-normal tracking-wide text-lime-950/60 md:text-4xl dark:text-stone-50/60 dark:selection:bg-stone-50 dark:selection:text-stone-950/60">
